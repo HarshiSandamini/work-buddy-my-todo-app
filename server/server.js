e69c46396cdf65568bfc3d6caba4a553c2ebf061
Harshika <sandamin-cs18038@stu.kln.ac.lk>
2023-08-01 15:39:27 +0530

    connection string added

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use("/todo",todoRouter);
 app.use("/user", userRouter)
 
 //mongoose.connect("mongodb+srv://Harshi:harshi@todowork.lvq4pdf.mongodb.net/?retryWrites=true&w=majority",
-mongoose.connect("mongodb+srv://Harshi:harshi@todowork.lvq4pdf.mongodb.net/?retryWrites=true&w=majority",
+mongoose.connect(" mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.3",
 {
     useNewUrlParser: true,
     useUnifiedTopology: true,
