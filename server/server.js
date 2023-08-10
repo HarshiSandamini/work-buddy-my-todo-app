f54c99bde0c0402d3846d78d59fc7d5019b49bf6
Harshika Sandamini <88420241+HarshiSandamini@users.noreply.github.com>
2023-08-10 17:27:31 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,6 @@ app.use("/todo",todoRouter);
 app.use("/user", userRouter)
 
 mongoose.connect("mongodb+srv://Username:<password>@todowork.lvq4pdf.mongodb.net/?retryWrites=true&w=majority",
-// mongoose.connect("mongodb://127.0.0.1:27017/work-buddy",
 {
     useNewUrlParser: true,
     useUnifiedTopology: true,
